d1e9f65e4596c138417b4ffb0837b9c73f4d5789
Petko Minkov <pminkov@twitter.com>
2019-02-10 20:20:39 -0800

    lucene

--- learnjava/src/java/com/pminkov/search/Search.java
+++ learnjava/src/java/com/pminkov/search/Search.java
@@ -1,14 +1,59 @@
 package com.pminkov.search;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.apache.lucene.document.Document;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.util.IOUtils;
 
 
 public class Search {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException, ParseException {
     StandardAnalyzer analyzer = new StandardAnalyzer();
 
-    System.out.println("Search.java");
+    Path indexPath = Files.createTempDirectory("tempIndex");
+
+    Directory directory = FSDirectory.open(indexPath);
+    IndexWriterConfig config = new IndexWriterConfig(analyzer);
+    IndexWriter iwriter = new IndexWriter(directory, config);
+
+    Document doc = new Document();
+    String text = "This is the text to be indexed.";
+    doc.add(new Field("fieldname", text, TextField.TYPE_STORED));
+    iwriter.addDocument(doc);
+    iwriter.close();
+
+    // Now search the index:
+    DirectoryReader ireader = DirectoryReader.open(directory);
+    IndexSearcher isearcher = new IndexSearcher(ireader);
+
+    QueryParser parser = new QueryParser("fieldname", analyzer);
+    Query query = parser.parse("tessdddxt");
+    ScoreDoc[] hits = isearcher.search(query, 10).scoreDocs;
+    System.out.println("Hits # = " + hits.length);
+    for (int i = 0; i < hits.length; i++) {
+      Document hitDoc = isearcher.doc(hits[i].doc);
+      System.out.println(hitDoc.get("fieldname"));
+    }
+    ireader.close();
+    directory.close();
+    IOUtils.rm(indexPath);
+
+    System.out.println("hello");
   }
 }
 
