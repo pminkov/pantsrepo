c3125f8d0ba97bab14d400261c78ff37df001f04
Petko Minkov <pminkov@twitter.com>
2019-05-08 10:21:11 -0700

    write posting list, get fields

--- search/src/java/com/pminkov/search/Search.java
+++ search/src/java/com/pminkov/search/Search.java
@@ -12,7 +12,13 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.LeafReaderContext;
+import org.apache.lucene.index.PostingsEnum;
+import org.apache.lucene.index.Term;
 import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.DocIdSetIterator;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
@@ -37,9 +43,16 @@ public class Search {
     QueryParser parser = new QueryParser("text", analyzer);
     Query query = parser.parse(queryString);
     ScoreDoc[] hits = isearcher.search(query, 10).scoreDocs;
+
     System.out.println("Hits # = " + hits.length);
+
     for (int i = 0; i < hits.length; i++) {
       Document hitDoc = isearcher.doc(hits[i].doc);
+
+      // Print all fields in this document.
+      for (IndexableField field : hitDoc.getFields()) {
+        System.out.println(field);
+      }
       System.out.println(hits[i].doc + " " + hitDoc.get("text"));
     }
   }
@@ -66,8 +79,22 @@ public class Search {
     StandardAnalyzer analyzer = new StandardAnalyzer();
     Directory directory = indexDocs(indexPath, analyzer);
 
-    // Now search the index:
+    // Load the index.
     DirectoryReader ireader = DirectoryReader.open(directory);
+
+    // Find the posting list for "dog".
+    for (LeafReaderContext leaf : ireader.leaves()) {
+      LeafReader leafReader = leaf.reader();
+      Term t = new Term("text", "dog");
+      PostingsEnum postings = leafReader.postings(t);
+      System.out.println("Postings for dog:");
+      int docId;
+      while ((docId = postings.nextDoc()) != DocIdSetIterator.NO_MORE_DOCS) {
+        System.out.print(docId + ", ");
+      }
+      System.out.println();
+    }
+
     IndexSearcher isearcher = new IndexSearcher(ireader);
 
     search("text: \"cat\" AND \"dog\"", isearcher, analyzer);
